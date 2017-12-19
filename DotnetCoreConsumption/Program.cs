cdbd5c227755a8e2b4f3cfc36ed12d19a8c8224e
René Rotvig Jensen <renerotvig@msn.com>
2017-12-19 11:33:28 +0100

    Project added

---
+++ DotnetCoreConsumption/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotnetCoreConsumption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
